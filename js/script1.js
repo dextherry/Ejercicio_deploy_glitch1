0b0dac7c0a12a85b63276b403d745d2292010459
giancarlo <giancarlo.ramos@janaq.com>
2019-07-12 14:42:10 -0500

    pruebaaa03

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,33 @@
 console.log("ESTE ES LA VERSION 17 DE SCRIPT1");
+
+
+
+document.getElementById('donde').innerHTML = document.location.href;
+
+
+
+
+
+
+var btn = document.getElementById('elbtna');
+var bio = document.getElementById('aquia');
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+  if(request.readyState === 4) {
+    bio.style.border = '1px solid #e8e8e8';
+    if(request.status === 200) {
+      bio.innerHTML = request.responseText;
+    } else {
+      bio.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+    }
+  }
+}
+
+request.open('get', 'https://jsonplaceholder.typicode.com/todos/2');
+
+btn.addEventListener('click', function() {
+  this.style.display = 'none';
+  request.send();
+});
